b78f4c3a10e6ee770c30f6ed0fa1c640f4b05bf3
EngrMuqeetAhmad <engrmuqeetahmad@gmail.com>
2026-07-19 19:39:30 +0500

    nodejs learning

---
+++ nodejs/index.js
@@ -0,0 +1,25 @@
+const person = {
+
+    name: "Muqeet",
+    age: 24,
+    education: "engineer"
+
+
+}
+
+
+//object
+
+// console.log(person)
+
+
+const allow = 'true' //datatype string
+
+
+if (allow === true) {
+    console.log("allow")
+} else {
+    console.log(
+        "not allow"
+    )
+}
